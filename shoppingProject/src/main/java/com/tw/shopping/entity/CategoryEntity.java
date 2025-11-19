1618f0bb9b65c08b4891a01b08bbbb344ba08f74
Sulayee <lin7371610@gmail.com>
2025-11-19 16:25:51 +0800

    11191625

---
+++ shoppingProject/src/main/java/com/tw/shopping/entity/CategoryEntity.java
@@ -0,0 +1,30 @@
+package com.tw.shopping.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Entity
+@Table(name = "category")
+@Data
+public class CategoryEntity {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "categoryid")
+	private Integer categoryId;
+	
+	@Column(name = "cname")
+	private String cname;
+	
+	@Column(name = "code")
+	private String code;
+	
+	@Column(name = "parentid")
+	private String parentId;
+	
+	@@
+}
